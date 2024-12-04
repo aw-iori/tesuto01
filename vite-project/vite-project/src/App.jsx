4bf44e97765a631d0e4231f31e8e14218a17e54f
saitou <24AW0117@jec.ac.jp>
2024-12-04 16:34:37 +0900

    2

---
+++ vite-project/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Profile.jsx'
+import Gallery from './Profile'
+
+function App() {
+  return (
+    <>
+      <Gallery/>
+    </>
+  )
+}
+
+export default App
